2ecc56397cdd3544e26221df338df6a429bc16c9
Alex Lundgren <alex@endem.su>
2015-11-22 21:21:39 +0300

    fx

--- core/Basic/Views.Class.php
+++ core/Basic/Views.Class.php
@@ -4,8 +4,6 @@ class Views {
 	
 	static function get ($name) {
 
-		use '\';
-
 		$dst = ROOT . '/resources/views/' . str_replace('.', '/', $name) . '.php';
 		if (!file_exists($dst)) {
 			throw new \Exception('Template ' . $dst . ' not exists', 1);
