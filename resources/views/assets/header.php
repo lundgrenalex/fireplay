533785b8fb960ea8782877ddad50eba17dd7fd79
Alex Lundgren <alex@endem.su>
2015-11-22 21:54:15 +0300

    fx

--- resources/views/assets/header.php
+++ resources/views/assets/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
 	<title>Fireplay Framework</title>
 </head>
 <body>
