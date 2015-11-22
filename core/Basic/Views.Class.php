05080a7490034d7ec70952c16ae3cecb6c65af61
Alex Lundgren <alex@endem.su>
2015-11-22 21:28:08 +0300

    fx

--- core/Basic/Views.Class.php
+++ core/Basic/Views.Class.php
@@ -1,12 +1,16 @@
 <?php
 
+use \Storage\Log as Log;
+
 class Views {
 	
 	static function get ($name) {
 
 		$dst = ROOT . '/resources/views/' . str_replace('.', '/', $name) . '.php';
 		if (!file_exists($dst)) {
-			throw new \Exception('Template ' . $dst . ' not exists', 1);
+			$message = 'Template ' . $dst . ' not exists';
+			Log::error($message);
+			throw new \Exception($message, 1);
 		}
 
 		include_once $dst;
