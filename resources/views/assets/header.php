5bfe19c72b64c21e1a375c6ca38b5c1dcc904e6d
Alex Lundgren <alex@endem.su>
2015-11-22 22:09:10 +0300

    fx

--- resources/views/assets/header.php
+++ resources/views/assets/header.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
 	<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+
 	<title>Fireplay Framework</title>
+
 </head>
 <body>
